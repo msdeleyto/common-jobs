b899abbfb919c44f94244f86300ea7cefe76d1c3
msdeleyto <msd117c@gmail.com>
2026-05-17 12:38:01 +0200

    skip pin digests for my actions

--- renovate/config.js
+++ renovate/config.js
@@ -1,5 +1,12 @@
 module.exports = {
   extends: ["helpers:pinGitHubActionDigests"],
+  packageRules: [
+    {
+      matchManagers: ["github-actions"],
+      matchPackageNames: ["msdeleyto/gh-actions"],
+      pinDigests: false,
+    },
+  ],
   repositories: [
     "msdeleyto/common-jobs",
     "msdeleyto/gh-actions",
