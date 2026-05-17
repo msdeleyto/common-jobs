60fcb9fbc6755bf876fe087e27cb138e5b585bbd
msdeleyto <msd117c@gmail.com>
2026-05-17 11:58:36 +0200

    configure renovate runs

---
+++ renovate/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ["helpers:pinGitHubActionDigests"],
+  repositories: [
+    "msdeleyto/gh-actions",
+    "msdeleyto/homelab-manifests",
+    "msdeleyto/qbittorrent-docker",
+    "msdeleyto/slskd-docker",
+  ],
+};
