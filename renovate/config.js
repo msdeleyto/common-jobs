1198be6350d9bdfb81971bc5d6b44781d4dfa065
msdeleyto <msd117c@gmail.com>
2026-05-17 12:08:38 +0200

    include current repo

--- renovate/config.js
+++ renovate/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ["helpers:pinGitHubActionDigests"],
   repositories: [
+    "msdeleyto/common-jobs",
     "msdeleyto/gh-actions",
     "msdeleyto/homelab-manifests",
     "msdeleyto/qbittorrent-docker",
